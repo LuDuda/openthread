a5b0f7c56aee49602a78e2f33fc0f073767e3e9b
Łukasz Duda <lukasz.duda@nordicsemi.no>
2025-05-07 00:19:43 +0200

    [crypto] PSA API: remove otPlatCryptoInit API
    This commit removes otPlatCryptoInit API and moves responsibility of
    initializing the Crypto subsystem to the platform.
    
    Signed-off-by: Łukasz Duda <lukasz.duda@nordicsemi.no>

--- src/core/crypto/crypto_platform_mbedtls.cpp
+++ src/core/crypto/crypto_platform_mbedtls.cpp
@@ -79,11 +79,6 @@ static constexpr uint16_t kEntropyMinThreshold = 16;
 #endif
 #endif
 
-OT_TOOL_WEAK void otPlatCryptoInit(void)
-{
-    // Intentionally empty.
-}
-
 // AES  Implementation
 OT_TOOL_WEAK otError otPlatCryptoAesInit(otCryptoContext *aContext)
 {
